d399ee5e862c45c4f95c39ec2d68937e8b3dbd0e
Napozz <nicolas.pelletierpro44@gmail.com>
2024-10-29 15:22:46 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+struct Todo {
+    list : Vec<String>
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    Add,
+    Delete,
+    Update,
+    
+}
+
+impl Todo {
+    
+}
+
+
+fn main() {
+    println!("hello world !")
+}
+
